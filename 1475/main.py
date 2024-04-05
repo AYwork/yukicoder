5c42c2249ae38ecdf2f615f781bc8973052cef1c
yoshiki abe <abeyoshiki@abesachikinoMacBook-Pro.local>
2024-04-05 15:58:52 +0900

    1475desu

---
+++ 1475/main.py
@@ -0,0 +1,11 @@
+def main():
+    N = int(input())
+    for i in range(N):
+        L = list(map(int, input().split()))[1:]
+        L = sorted(L, reverse=True)
+        L = ' '.join(map(str,L))
+        print(L)
+
+
+if __name__ == "__main__":
+    main()
