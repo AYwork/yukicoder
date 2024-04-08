9d3fbb51f955ec19bdcce7f32d28a15ecb267fa9
yoshiki abe <abeyoshiki@abesachikinoMacBook-Pro.local>
2024-04-08 10:48:44 +0900

    1335resubmit

--- 1335/main.py
+++ 1335/main.py
@@ -1,32 +1,15 @@
 def main():
-    A = input()
-    S = input()
-    T = []
-    for x in S:
-        if x == "0":
-            T.append(A[0])
-        elif x == "1":
-            T.append(A[1])
-        elif x == "2":
-            T.append(A[2]) 
-        elif x == "3":
-            T.append(A[3]) 
-        elif x == "4":
-            T.append(A[4]) 
-        elif x == "5":
-            T.append(A[5])  
-        elif x == "6":
-            T.append(A[6])  
-        elif x == "7":
-            T.append(A[7])      
-        elif x == "8":
-            T.append(A[8])      
-        elif x == "9":
-            T.append(A[9])
-        else:
-            T.append(x)
-    T = "".join(T) 
-    print(T)
+    a = input()
+    s = input()
+    t = []
+    for x in s:
+        try:
+            x=int(x)
+            t.append(a[x])
+        except:
+            t.append(x)
+    t = "".join(t) 
+    print(t)
 
 if __name__ == "__main__":
     main()
