b621ceb67bb0650736dcc6e15f179cefb296adb1
yoshiki abe <abeyoshiki@abesachikinoMacBook-Pro.local>
2024-04-08 12:24:55 +0900

    784resubmit

--- 784/main.py
+++ 784/main.py
@@ -1,13 +1,12 @@
 def main():
     num=input()
     dig=len(num)
-    if dig>=4:
-        num = num[:-3] + ',' + num[-3:]
-    if dig>=7:
-        num = num[:-7] + ',' + num[-7:]
-    if dig>=10:
-        num = num[:-11] + ',' + num[-11:]
-    print (num)
+    ans=[]
+    for i in range(dig-1,-1,-1):
+        ans.append(num[i])
+        if (dig-i)%3==0 and i!=0:
+            ans.append(",")
+    print("".join(ans)[::-1])
 
 if __name__ == "__main__":
     main()
