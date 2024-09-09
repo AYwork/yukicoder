220d629629e037eddcca7bfe69721eeed7dcb767
yoshiki abe <abeyoshiki@abesachikinoMacBook-Pro-2.local>
2024-09-09 19:27:02 +0900

    9/9

---
+++ ATCODER/knapsack.py
@@ -0,0 +1,21 @@
+#https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_s
+
+def main():
+    count,  weight_limit = map(int, input().split())
+
+    weight = [0]
+    value = [0]
+    for i range(N):
+        a, b = map(int, input().split())
+        weight.append(a)
+        value.append(b)
+
+#dp遷移式というものを使うらしい
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
